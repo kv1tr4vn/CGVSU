2d0e39357461c90f5437f2f0b2f3e2d6ea51ecbc
kv1tr4vn <kv1tr4vn@gmail.com>
2023-09-03 15:31:04 +0300

    Add Simple3DViewer from old repo

---
+++ Task4/Simple3DViewer/src/main/java/com/cgvsu/GuiController.java
@@ -0,0 +1,118 @@
+package com.cgvsu;
+
+import com.cgvsu.render_engine.RenderEngine;
+import javafx.fxml.FXML;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.FileChooser;
+import javafx.util.Duration;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
+import java.io.File;
+import javax.vecmath.Vector3f;
+
+import com.cgvsu.model.Model;
+import com.cgvsu.objreader.ObjReader;
+import com.cgvsu.render_engine.Camera;
+
+public class GuiController {
+
+    final private float TRANSLATION = 0.5F;
+
+    @FXML
+    AnchorPane anchorPane;
+
+    @FXML
+    private Canvas canvas;
+
+    private Model mesh = null;
+
+    private Camera camera = new Camera(
+            new Vector3f(0, 00, 100),
+            new Vector3f(0, 0, 0),
+            1.0F, 1, 0.01F, 100);
+
+    private Timeline timeline;
+
+    @FXML
+    private void initialize() {
+        anchorPane.prefWidthProperty().addListener((ov, oldValue, newValue) -> canvas.setWidth(newValue.doubleValue()));
+        anchorPane.prefHeightProperty().addListener((ov, oldValue, newValue) -> canvas.setHeight(newValue.doubleValue()));
+
+        timeline = new Timeline();
+        timeline.setCycleCount(Animation.INDEFINITE);
+
+        KeyFrame frame = new KeyFrame(Duration.millis(15), event -> {
+            double width = canvas.getWidth();
+            double height = canvas.getHeight();
+
+            canvas.getGraphicsContext2D().clearRect(0, 0, width, height);
+            camera.setAspectRatio((float) (width / height));
+
+            if (mesh != null) {
+                RenderEngine.render(canvas.getGraphicsContext2D(), camera, mesh, (int) width, (int) height);
+            }
+        });
+
+        timeline.getKeyFrames().add(frame);
+        timeline.play();
+    }
+
+    @FXML
+    private void onOpenModelMenuItemClick() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Model (*.obj)", "*.obj"));
+        fileChooser.setTitle("Load Model");
+
+        File file = fileChooser.showOpenDialog((Stage) canvas.getScene().getWindow());
+        if (file == null) {
+            return;
+        }
+
+        Path fileName = Path.of(file.getAbsolutePath());
+
+        try {
+            String fileContent = Files.readString(fileName);
+            mesh = ObjReader.read(fileContent);
+            // todo: обработка ошибок
+        } catch (IOException exception) {
+
+        }
+    }
+
+    @FXML
+    public void handleCameraForward(ActionEvent actionEvent) {
+        camera.movePosition(new Vector3f(0, 0, -TRANSLATION));
+    }
+
+    @FXML
+    public void handleCameraBackward(ActionEvent actionEvent) {
+        camera.movePosition(new Vector3f(0, 0, TRANSLATION));
+    }
+
+    @FXML
+    public void handleCameraLeft(ActionEvent actionEvent) {
+        camera.movePosition(new Vector3f(TRANSLATION, 0, 0));
+    }
+
+    @FXML
+    public void handleCameraRight(ActionEvent actionEvent) {
+        camera.movePosition(new Vector3f(-TRANSLATION, 0, 0));
+    }
+
+    @FXML
+    public void handleCameraUp(ActionEvent actionEvent) {
+        camera.movePosition(new Vector3f(0, TRANSLATION, 0));
+    }
+
+    @FXML
+    public void handleCameraDown(ActionEvent actionEvent) {
+        camera.movePosition(new Vector3f(0, -TRANSLATION, 0));
+    }
+}
---
+++ Task4/Simple3DViewer/src/main/java/com/cgvsu/Main.java
@@ -0,0 +1,7 @@
+package com.cgvsu;
+
+public class Main {
+    public static void main(String[] args) {
+        Simple3DViewer.main(args);
+    }
+}
